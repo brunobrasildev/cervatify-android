6dc3f527f86374cc434f150ebcc2036e56aef0bd
Aluno <aluno@academico.local>
2017-11-08 21:04:13 -0200

    Atualizar

---
+++ app/src/androidTest/java/brunobrasilweb/com/br/cervatify/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package brunobrasilweb.com.br.cervatify;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("brunobrasilweb.com.br.cervatify", appContext.getPackageName());
+    }
+}
